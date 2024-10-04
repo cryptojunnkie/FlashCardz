642b447d33a536dfa20bf521a7abc4673e977a84
cryptojunnkie <44417606+cryptojunnkie@users.noreply.github.com>
2024-10-04 18:32:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,10 +92,8 @@ flashcards = {
         )
     },
     "EXAM NOTE" :{  # New section for section summary
-        "": (
-            "<ul>"
-            "<li>Remember the main components of the CIA triad security model are confidentiality, integrity, and availability. Also know that nonrepudiation is the assurance that something cannot be denied by someone.</li>"
-            "</ul>"
+        "Remember the main components of the CIA triad security model are confidentiality, integrity, and availability. Also know that nonrepudiation is the assurance that something cannot be denied by someone.": (
+            ""
         )
     },
 }
